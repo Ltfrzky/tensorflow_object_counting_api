241af67cbcc58e5129b9d3b3340d7baee5495583
Lutfi <ltfrzky@gmail.com>
2019-08-21 00:52:14 +0700

    removes unnecessary files

--- vehicle_counting.py
+++ vehicle_counting.py
@@ -6,18 +6,26 @@
 
 # Imports
 import tensorflow as tf
+import argparse
 
 # Object detection imports
 from utils import backbone
 from api import object_counting_api
 
-input_video = "./input_images_and_videos/vehicle_survaillance.mp4"
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--input", required=True,
+	help="path to input video")
+ap.add_argument("-r", "--roi", required=True,
+	help="ROI Line")
+args = vars(ap.parse_args())
+
+input_video = args["input"]
 
 # By default I use an "SSD with Mobilenet" model here. See the detection model zoo (https://github.com/tensorflow/models/blob/master/research/object_detection/g3doc/detection_model_zoo.md) for a list of other models that can be run out-of-the-box with varying speeds and accuracies.
-detection_graph, category_index = backbone.set_model('ssd_mobilenet_v1_coco_2018_01_28', 'mscoco_label_map.pbtxt')
+detection_graph, category_index = backbone.set_model('mobilenet_v2_1.4_224', 'mscoco_label_map.pbtxt')
 
 is_color_recognition_enabled = 1 # set it to 1 for enabling the color prediction for the detected objects
-roi = 185 # roi line position
+roi = args["roi"] # roi line position
 deviation = 2 # the constant that represents the object counting area
 
 object_counting_api.cumulative_object_counting_y_axis(input_video, detection_graph, category_index, is_color_recognition_enabled, roi, deviation) # counting all the objects
